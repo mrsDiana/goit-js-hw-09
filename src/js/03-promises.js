41017afa0ca987703f5d1bdbf3fb42499fd95b51
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-11-02 21:49:36 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handelSubmit);
+
+function handelSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  let delayCounter = Number(delay.value);
+
+  for (let position = 0; position < amount.value; position += 1) {
+    createPromise(position, delayCounter)
+      .then(({ position, delayCounter }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delayCounter}ms`
+        );
+      })
+      .catch(({ position, delayCounter }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delayCounter}ms`
+        );
+      });
+    delayCounter += Number(step.value);
   }
+}
+
+function createPromise(position, delayCounter) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delayCounter}ms`);
+        // Fulfill
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delayCounter}ms`); // Reject
+      }
+    }, delayCounter);
+  });
+
+  return promise;
 }
